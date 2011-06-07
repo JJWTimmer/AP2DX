954579801c9df8dbe6deec3d4644924a22edfe53
Jasper Timmer <jjwtimmer@gmail.com>
2011-06-07 12:08:31 +0200

    java workspace

---
+++ java/AP2DX/src/AP2DX/AP2DXBase.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package AP2DX;
+
+/**
+ * @author jjwt
+ *
+ */
+public abstract class AP2DXBase {
+	public static void main (String[] args){
+
+	}
+}
---
+++ java/AP2DX/src/AP2DX/coordinator/main.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package AP2DX.coordinator;
+
+import AP2DX.AP2DXBase;
+
+/**
+ * @author jjwt
+ *
+ */
+public class main extends AP2DXBase {
+
+}
