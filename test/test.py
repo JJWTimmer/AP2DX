457f5f7f91a42776d58c507d8f082a798d4c2b92
Maarten Inja <maarten.inja@gmail.com>
2011-06-07 15:40:50 +0200

    more sensordata

--- test/test.py
+++ test/test.py
@@ -9,24 +9,24 @@ class test:
 
     def __init__(self):
         #TCP_IP = "146.50.51.85"
-        TCP_IP = "146.50.51.85"
-        TCP_PORT = 3000
-        BUFFER_SIZE = 1024
+        tcpIp = "146.50.51.85"
+        tcpPort = 3000
+        bufferSize = 1024
         spawnMessage = "INIT {ClassName USARBot.P2DX} {Location 4.5,1.9,1.8} {Name R1}\r\n"
         forwardDriveMessage = "DRIVE {Left 1} {Right 1} {Normalized true}\r\n"
        
         print "attempting to connect"
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
-        data = s.recv(BUFFER_SIZE)
+        s.connect((tcpIp, tcpPort))
+        data = s.recv(bufferSize)
         print "done, data:\n%s" % data
         print "attempting to send spawn message"
         s.send(spawnMessage)
-        data = s.recv(BUFFER_SIZE)
+        data = s.recv(bufferSize)
         print "attempting to send forward drive message"
         while True:
             s.send(forwardDriveMessage)
-            data = s.recv(BUFFER_SIZE)
+            data = s.recv(bufferSize)
             print data
 
         s.close()
