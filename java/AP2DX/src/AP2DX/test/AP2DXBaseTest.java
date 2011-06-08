e68be627f175de0eecad54045664bd79a28ab2ec
Jasper Timmer <jjwtimmer@gmail.com>
2011-06-08 17:00:00 +0200

    new test added again

--- java/AP2DX/src/AP2DX/test/AP2DXBaseTest.java
+++ java/AP2DX/src/AP2DX/test/AP2DXBaseTest.java
@@ -3,6 +3,7 @@
  */
 package AP2DX.test;
 
+import AP2DX.AP2DXBase;
 import junit.framework.TestCase;
 
 /**
@@ -13,6 +14,17 @@ public class AP2DXBaseTest extends TestCase {
 
     public void testNothing() {
     	assertEquals(1, 1);
+    }
+    
+    public void testConcreteClass() {
+    	AP2DXBase test = new ConcreteClass();
+    	assertNotNull(test);
+    	
+    }
+    
+    private class ConcreteClass extends AP2DXBase {
+    	//empty
+    	
     }
     
 }
