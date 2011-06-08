414d5d26b171327f6c49b2799227363cba20bf41
Jasper Timmer <jjwtimmer@gmail.com>
2011-06-08 16:38:08 +0200

    again minor changes

---
+++ java/AP2DX/src/AP2DX/test/AP2DXBaseTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package AP2DX.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Jasper
+ *
+ */
+public class AP2DXBaseTest extends TestCase {
+
+    public void testNothing() {
+    	assertEquals(1, 1);
+    }
+    
+}
