944ed759ffc2ab4b530cf41e55835d85e63acce8
Jasper Timmer <jjwtimmer@gmail.com>
2011-06-09 17:56:55 +0200

    expansion of Base

--- java/AP2DX/src/AP2DX/AP2DXBase.java
+++ java/AP2DX/src/AP2DX/AP2DXBase.java
@@ -4,6 +4,8 @@
 package AP2DX;
 
 import java.io.*;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.logging.*;
 import java.util.Map;
 import java.util.ArrayList;
@@ -24,57 +26,78 @@ import org.json.simple.parser.ParseException;
  * Can send to socket[s]
  */
 
-public abstract class AP2DXBase extends Thread{
+public abstract class AP2DXBase {
 	
 	public static Logger logger;
     /** The configuration read from file and set by readConfig.*/
     protected Map config;
+    private Logic BaseLogic;
     
-    static private ArrayList<Connection> connections = new ArrayList<Connection>();
+    private ArrayList<ConnectionHandler> inConnections = new ArrayList<ConnectionHandler>();
+    private ArrayList<ConnectionHandler> outConnections = new ArrayList<ConnectionHandler>();
 
+    public ArrayBlockingQueue<Message> ReceiveQueue = new ArrayBlockingQueue<Message>(128);
+    
 	/**
 	 * constructor
 	 */
-	public AP2DXBase() {
+	public AP2DXBase(Module module) {
 		config = readConfig();
-        setConfig(); 
-	    try {
-	      boolean append = true;
-	      FileHandler fh = new FileHandler(config.get("logfile").toString(), append);
-	      
-	      //set logfile format:
-	      //date millis methodname level message
-	      fh.setFormatter(new Formatter() {
-	          public String format(LogRecord rec) {
-	             StringBuffer buf = new StringBuffer(1000);
-	             buf.append(new java.util.Date());
-	             buf.append(' ');
-	             buf.append(rec.getMillis());
-	             buf.append(' ');
-	             buf.append(rec.getSourceMethodName());
-	             buf.append(' ');
-	             buf.append(rec.getLevel());
-	             buf.append(' ');
-	             buf.append(formatMessage(rec));
-	             buf.append('\n');
-	             return buf.toString();
-	             }
-	           });
-
-	      logger = Logger.getLogger(this.getClass().getPackage().getName());
-	      logger.addHandler(fh);
-	    }
-	    catch (IOException e) {
-	      e.printStackTrace();
-	    }
+        
+		setConfig();
+	    
+        initLogger();
 	    
 	    logger.info("Package: " + this.getClass().getPackage().getName());
 	    logger.info("Directory: " + System.getProperty("user.dir"));
 	    
+	    BaseLogic = new Logic(this);
+	    
+	    BaseLogic.run();
+	    
+	    
 	    /**
 	     * Establish all the connections.
 	     */
-	    //CODE
+	    //Outgoing connections
+	    	    
+	    
+	    //Incoming connections
+	}
+	
+	/**
+	 * starts a logger, defines a format
+	 */
+	private void initLogger() {
+		try {
+		      boolean append = true;
+		      FileHandler fh = new FileHandler(config.get("logfile").toString(), append);
+		      
+		      //set logfile format:
+		      //date millis methodname level message
+		      fh.setFormatter(new Formatter() {
+		          public String format(LogRecord rec) {
+		             StringBuffer buf = new StringBuffer(1000);
+		             buf.append(new java.util.Date());
+		             buf.append(' ');
+		             buf.append(rec.getMillis());
+		             buf.append(' ');
+		             buf.append(rec.getSourceMethodName());
+		             buf.append(' ');
+		             buf.append(rec.getLevel());
+		             buf.append(' ');
+		             buf.append(formatMessage(rec));
+		             buf.append('\n');
+		             return buf.toString();
+		             }
+		           });
+
+		      logger = Logger.getLogger(this.getClass().getPackage().getName());
+		      logger.addHandler(fh);
+		    }
+		    catch (IOException e) {
+		      e.printStackTrace();
+		    }
 	}
 	
 	
@@ -142,37 +165,66 @@ public abstract class AP2DXBase extends Thread{
 	    return contents.toString();
 	  }
 	
-	
-	public boolean UpdateConnection (Connection connection) {
-		Connection conn;
-		for (int i = 0; i < connections.size(); i++) {
-			conn = connections.get(i);
-			if (conn.GetModule().compareTo(connection.GetModule()) == 0) {
-				connections.set(i, connection);
+	/**
+	 * Compares old connection's module with new connection's module.
+	 * Replaces connection object for the same module.
+	 * @author Wadie Assal
+	 * @param connection
+	 * @return
+	 */
+	public boolean UpdateIncomingConnection  (ConnectionHandler connection) {
+		ConnectionHandler conn;
+		for (int i = 0; i < inConnections.size(); i++) {
+			conn = inConnections.get(i);
+			if (conn.moduleID.compareTo(connection.moduleID) == 0) {
+				inConnections.set(i, connection);
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	public Connection GetConnection (Module module) throws Exception{
-		Connection conn;
-		for (int i = 0; i < connections.size(); i++) {
-			conn = connections.get(i);
-			if (conn.GetModule().compareTo(module) == 0) {
+	/**
+	 * Compares old connection's module with new connection's module.
+	 * Replaces connection object for the same module.
+	 * @author Wadie Assal
+	 * @param connection
+	 * @return
+	 */
+	public boolean UpdateOutgoingConnection (ConnectionHandler connection) {
+		ConnectionHandler conn;
+		for (int i = 0; i < outConnections.size(); i++) {
+			conn = outConnections.get(i);
+			if (conn.moduleID.compareTo(connection.moduleID) == 0) {
+				outConnections.set(i, connection);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public ConnectionHandler GetSendConnection (Module module) throws Exception{
+		ConnectionHandler conn;
+		for (int i = 0; i < outConnections.size(); i++) {
+			conn = outConnections.get(i);
+			if (conn.moduleID.compareTo(module) == 0) {
 				return conn;
 			}
 		}
 		return null;
 	}
 	
-	private boolean AddConnection (String ipaddress, int port, Module module){
+	private boolean AddSendConnection (String ipaddress, int port, Module module){
 		try {
-			Connection conn = new Connection(ipaddress, port, module);
-			connections.add(conn);
+			Socket sock = new Socket(ipaddress, port);
+			ConnectionHandler conn = new ConnectionHandler(this, sock, module);
+			outConnections.add(conn);
 			return true;
 		} catch (Exception e) {
 			return false;
 		}
 	}
+	
+	public abstract ArrayList<Message> ComponentLogic(Message msg);
 }
