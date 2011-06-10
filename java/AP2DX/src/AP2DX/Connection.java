0bebf6d8528b3a105f9f0c6c2941d62a57c86114
Wadie Assal <assalw@gmail.com>
2011-06-10 16:36:13 +0200

    Added comments

--- java/AP2DX/src/AP2DX/Connection.java
+++ java/AP2DX/src/AP2DX/Connection.java
@@ -4,74 +4,61 @@ import java.net.*;
 import java.io.*;
 
 /*
-* @author Maarten Inja
-* A class that should handle all a socket connection to both send and receive messages. The receiving part might be difficult
-* since this could happen all the time, or not at all.
-*/
-public class Connection
-{
+ * @author Maarten Inja
+ * A class that should handle all a socket connection to both send and receive messages. The receiving part might be difficult
+ * since this could happen all the time, or not at all.
+ */
+public class Connection {
 
 	private InetAddress address;
 	private Socket socket;
 
-    private PrintWriter out;
-    private BufferedReader in;
-    private Module module;
+	private PrintWriter out;
+	private BufferedReader in;
+	private Module module;
 
-    public Connection(String addressString, int port, Module module) throws Exception
-    {
-    	this.module = module;
-        address = InetAddress.getByName(addressString);
-        socket = new Socket(address, port);
-        out = new PrintWriter(socket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    }
+	public Connection(String addressString, int port, Module module)
+			throws Exception {
+		this.module = module;
+		address = InetAddress.getByName(addressString);
+		socket = new Socket(address, port);
+		out = new PrintWriter(socket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
 
-    /** Sends a message to whatever this thing is connected to. */
-    public boolean sendMessage(String message)
-    {
-        try 
-        {
-        	out.print(message);
-            return true;
-        } 
-        catch (Exception e) 
-        {
-            return false;
-        }
-    }    
+	/** Sends a message to whatever this thing is connected to. */
+	public boolean sendMessage(String message) {
+		try {
+			out.print(message);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
-    public String readMessage()
-    {
-        try
-        {
-            return in.readLine();
-        }
-        catch (Exception e)
-        {   
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public String readMessage() {
+		try {
+			return in.readLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    /** Closes all connections. */
-    public boolean close()
-    {
-        try
-        {
-            out.close();
-            in.close(); 
-            socket.close();
-            return true;
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    
-    public Module GetModule () {
-    	return module;
-    }
+	/** Closes all connections. */
+	public boolean close() {
+		try {
+			out.close();
+			in.close();
+			socket.close();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public Module GetModule() {
+		return module;
+	}
 }
--- java/AP2DX/src/AP2DX/Module.java
+++ java/AP2DX/src/AP2DX/Module.java
@@ -1,5 +1,10 @@
 package AP2DX;
 
+/*
+ * Enum type for the different distributed modules of the this software.
+ * 
+ * @author Wadie Assal
+ */
 public enum Module {
     COORDINATOR, ABSTRACTMOTOR, REFLEX, SENSOR, 
     PLANNER, MAPPER, USARSIM, TEST, ADMIN
