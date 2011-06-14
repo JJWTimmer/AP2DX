bc8a150d56027e33a6249657e7d0ef0a9432348e
Wadie Assal <assalw@gmail.com>
2011-06-14 17:58:31 +0200

    classes got untracked because of a name change.

---
+++ java/AP2DX/src/AP2DX/reflex/Program.java
@@ -0,0 +1,41 @@
+package AP2DX.reflex;
+
+import java.util.ArrayList;
+
+import AP2DX.AP2DXBase;
+import AP2DX.Message;
+import AP2DX.Module;
+
+public class Program extends AP2DXBase {
+    /**
+	 * Entrypoint of reflex
+	 */
+	public static void main (String[] args){
+		new Program();
+		
+		System.exit(0);
+	}
+
+	
+	/**
+	 * constructor
+	 */
+	public Program() 
+    {
+        super(Module.REFLEX); // explicitly calls base constructor
+		System.out.println(" Running Reflex... ");
+
+	}
+	
+	@Override
+	protected void doOverride() {
+
+	}
+
+
+	@Override
+	public ArrayList<Message> componentLogic(Message msg) {
+		// TODO Auto-generated method stub
+		return new ArrayList<Message>();
+	}
+}
---
+++ java/AP2DX/src/AP2DX/sensor/Program.java
@@ -0,0 +1,41 @@
+package AP2DX.sensor;
+
+import java.util.ArrayList;
+
+import AP2DX.AP2DXBase;
+import AP2DX.Message;
+import AP2DX.Module;
+
+public class Program extends AP2DXBase {
+    /**
+	 * Entrypoint of mapper
+	 */
+	public static void main (String[] args){
+		new Program();
+		
+		System.exit(0);
+	}
+
+	
+	/**
+	 * constructor
+	 */
+	public Program() 
+    {
+        super(Module.SENSOR); // explicitly calls base constructor
+		System.out.println(" Running Sensor... ");
+
+	}
+	
+	@Override
+	protected void doOverride() {
+
+	}
+
+
+	@Override
+	public ArrayList<Message> componentLogic(Message msg) {
+		// TODO Auto-generated method stub
+		return new ArrayList<Message>();
+	}
+}
