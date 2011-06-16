2f627426fc923ea1c901d3a7fb80c163b6a3c7bd
Wadie Assal <assalw@gmail.com>
2011-06-16 18:15:57 +0200

    Fixed errors

--- java/AP2DX/src/AP2DX/reflex/Program.java
+++ java/AP2DX/src/AP2DX/reflex/Program.java
@@ -39,31 +39,6 @@ public class Program extends AP2DXBase {
 	public ArrayList<Message> componentLogic(Message msg) {
 		/* Checks from witch module the message comes from */
 		if (msg.getType().equals("")) {
-			/* 
-			 * Probably have to be changed in the future. 
-			 * Assumes there is an action value. 
-			 * Then converts it form string to enum ActionType.
-			 */
-			ActionMotorMessage.ActionType action = ActionMotorMessage.ActionType.valueOf(msg.getValues().get("action").toString());
-			
-			/* 
-			 * Switches to decide witch action to take.
-			 * Uses the enum ActionType to switch.
-			 */
-			switch (action) {
-			case FORWARD :
-				return reflex.forward(0);
-			case BACKWARD :
-				return reflex.backward(0);
-			case LEFT :
-				return reflex.left(0);
-			case RIGHT :
-				return reflex.right(0);
-			case TURN :
-				return reflex.turn(0);
-			case STOP :
-				return reflex.stop();
-			}
 			
 			return new ArrayList<Message>();			
 		} else if (msg.getType().equals("sensor")) {
