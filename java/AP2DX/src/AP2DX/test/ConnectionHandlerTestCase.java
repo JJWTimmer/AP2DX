7db35100381d617690322b83b02793e88af832f8
Maarten de Waard <mrtndwrd@gmail.com>
2011-06-17 18:57:28 +0200

    Added forgotten files

---
+++ java/AP2DX/src/AP2DX/test/ConnectionHandlerTestCase.java
@@ -0,0 +1,199 @@
+/*
+ * 
+ */
+package AP2DX.test;
+
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockClass;
+import mockit.MockUp;
+import mockit.Mockit;
+import static mockit.Deencapsulation.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.*;
+
+import AP2DX.*;
+
+import java.io.*;
+import java.lang.Thread;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * @author Maarten de Waard
+ */
+public class ConnectionHandlerTestCase{
+
+    private ConnectionHandler test;
+    private ConcreteBase fakeBase;
+    private Socket socket;
+
+    @BeforeClass 
+        public static void beforeClass() throws Exception 
+        {
+            Mockit.setUpMocks(FakeBase.class);
+            new MockUp<Socket>() 
+            {
+                @Mock
+                    void $init(String host, int port) 
+                    {
+                        System.out.printf("Host: %s, Port: %d\n", host, port);
+                        //pass
+                    }
+                @Mock
+                    public InputStream getInputStream()
+                    {   
+                        //TODO: Fix this to have the right type
+                        //return new BufferedReader(new StringReader("HELLO WORLD!!!\n"));
+                        return new StringBufferInputStream("HELLO WORLD");
+                    }
+                @Mock 
+                    public boolean isClosed()
+                    {
+                        return false;
+                    }
+                @Mock
+                    public OutputStream getOutputStream()
+                    {
+                        return null;
+                    }
+            };
+        }
+    @Before
+        public void before() throws Exception
+        {
+            //Instantiate the fake base
+            fakeBase = new ConcreteBase(Module.TEST);
+            //instantiate some socket
+            socket = new Socket("Host", 5999);
+            //Standardly, use a ConnectionHandler with no usar, 
+            //after the Todo in the mocked getInputStream is done, we can uncomment this again.
+            test = new ConnectionHandler(false, fakeBase, socket, Module.TEST, Module.TEST);
+        }
+
+    @Test
+        public void testConstructorFalse()
+        {
+            try{
+                ConnectionHandler myTest = new ConnectionHandler(false, fakeBase, socket, Module.TEST, Module.TEST);
+                assertNotNull(myTest);
+            }
+            catch(Exception e)
+            {
+                fail("Error in connectionhandler with false as argument");
+            }
+        }
+
+    @Test
+        public void testConstructorTrue()
+        {
+            try
+            {
+                ConnectionHandler myTest = new ConnectionHandler(true, fakeBase, socket, Module.TEST, Module.TEST);
+                assertNotNull(myTest);
+            }
+            catch(Exception e)
+            {
+                fail("Error in connectionhandler with true as argument");
+            }
+        }
+
+    @Test
+        public void testOtherConstructor()
+        {
+            try{
+                ConnectionHandler myTest = new ConnectionHandler(fakeBase, socket, Module.TEST);
+                assertNotNull(myTest);
+            }
+            catch(Exception e)
+            {
+                fail("Error in short connectionhandler");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @MockClass(realClass = AP2DXMessage.class)
+        public static final class FakeAP2DXMessage 
+        {
+            @Mock
+                public void $init(String in, Module origin)
+                {
+                    System.out.printf("In: %s, Module: %s", in, origin);
+                }
+
+            @Mock
+                public void parseMessage()
+                {
+                    System.out.println("parsing message");
+                }
+
+        }
+
+
+    /**
+     * This is the mocked AP2DXBase.
+     * 
+     * @author Jasper
+     * 
+     */
+    @MockClass(realClass = AP2DXBase.class)
+        public static final class FakeBase 
+        {		
+            /**
+             * Override the constructor of AP2DXBase to do nothing.
+             */
+            @Mock
+                public void $init(Module module) {
+                    //instead of connecting and configuring, do nothing
+                    System.out.printf("Module: %s\n", module);
+                }
+        }
+
+    public class ConcreteBase extends AP2DXBase {
+
+        public ConcreteBase(Module module)
+        {
+            super(module);
+        }
+
+        public Map getConfig() 
+        {
+            return this.config;
+        }
+
+        @Override
+            public ArrayList<AP2DXMessage> componentLogic(Message msg) 
+            {
+                return null;
+            }
+
+    }
+}
+
+
+
---
+++ java/AP2DX/src/AP2DX/usarsim/specialized/DriveMessage.java
@@ -0,0 +1,111 @@
+/**
+ * 
+ */
+package AP2DX.usarsim.specialized;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import AP2DX.Message;
+import AP2DX.usarsim.UsarSimMessage;
+import AP2DX.usarsim.UsarSimMessage.UsarMessageField;
+
+/**
+ * The message that will be sent to the robot to make it move.
+ * @author Maarten de Waard
+ */
+public final class DriveMessage extends UsarSimMessage {
+
+    public DriveMessage()
+    {
+        super(Message.MessageType.USAR_DRIVE);
+    }
+
+    /** Spin speed of the left side wheel. Has a value between -100 and 100, 
+     * corresponds to AP2DX' minimum and maximum spin speed. If `Normalized' is False, 
+     * this will be the spin speed in Radians per Second
+     */
+	@UsarMessageField(name = "Left")
+	private float left;
+
+    /** Same as left, but for the right wheel */
+    @UsarMessageField(name = "Right")
+    private float right;
+    
+    /** If this is set to true (standard is false) the value of left and right should be between -100 and 100.
+     * If it is set to False, left and right controll the wheel spin speed in radians per second.
+     */
+    @UsarMessageField(name = "Normalized")
+    private boolean normalized;
+
+    /** True to turn the lights on, false to turn them off */
+    @UsarMessageField(name = "Light")
+    private boolean light;
+
+    /** Will flip the robot to stand wheels-down on the floor */
+    @UsarMessageField(name = "Flip")
+    private boolean flip;
+    
+    /** Setter for the left wheel speed */
+    public  void setLeft(float leftIn)
+    {
+        this.left = leftIn;
+    }
+
+    /** Setter for the right wheel speed */
+    public  void setRight(float rightIn)
+    {
+        this.right = rightIn;
+    }
+
+    /** Setter for normalized */
+    public  void setNormalized(boolean norm)
+    {
+        this.normalized = norm;
+    }
+
+    /** Setter for light */
+    public  void setLight(boolean lightIn)
+    {
+        this.light = lightIn;
+    }
+
+    /** Setter for flip */
+    public  void setFlip(boolean flipIn)
+    {
+        // This flip is flippin'!
+        this.flip = flipIn;
+    }
+
+    /** Getter for right wheel speed */
+    public  float getRight()
+    {
+        return this.right;
+    }
+
+    /** Getter for left wheel speed */
+    public  float getLeft()
+    {
+        return this.left;
+    }
+
+    /** Getter for normalized*/
+    public  boolean getNormalized()
+    {
+        return this.normalized;
+    }
+
+    /** Getter for light*/
+    public  boolean getLight()
+    {
+        return this.light;
+    }
+
+    /** Getter for normalized*/
+    public  boolean getFlip()
+    {
+        return this.flip;
+    }
+}
