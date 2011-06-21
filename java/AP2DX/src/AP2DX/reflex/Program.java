91ab91327a6642e8e64bba2673f0cceaad2485df
Jasper Timmer <jjwtimmer@gmail.com>
2011-06-21 16:55:09 +0200

    text about our demo

--- java/AP2DX/src/AP2DX/reflex/Program.java
+++ java/AP2DX/src/AP2DX/reflex/Program.java
@@ -1,9 +1,7 @@
 package AP2DX.reflex;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.DelayQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import AP2DX.AP2DXBase;
 import AP2DX.AP2DXMessage;
@@ -14,6 +12,11 @@ import AP2DX.usarsim.specialized.USonarSensorMessage;
 
 public class Program extends AP2DXBase 
 {	
+	private double[] currentDistances;
+	private double[] previousDistances;
+	private double[] approaching;
+	private boolean isBotBlocked = false;
+	private ArrayBlockingQueue<AP2DXMessage> motorBacklog = new ArrayBlockingQueue<AP2DXMessage>(256);
 	
     /**
 	 * Entrypoint of reflex
@@ -50,14 +53,43 @@ public class Program extends AP2DXBase
         switch(message.getMsgType())
         {
             case AP2DX_MOTOR_ACTION:
-                //TODO: check command and override if nessecary
-            	// for now, we will just forward stuff to the motor, okay!?
+
+	    		ArrayBlockingQueue<AP2DXMessage> backlog = getMotorBacklog();
+	    		backlog.add((AP2DXMessage) message);
+	    		setMotorBacklog(backlog);
             	
-                message.setDestinationModuleId(Module.MOTOR);
-                messageList.add((AP2DXMessage)message);            
                 break;
             case AP2DX_SENSOR_SONAR:
             	USonarSensorMessage msg = (USonarSensorMessage)message;
+            	
+            	setPreviousDistances(getCurrentDistances());
+            	setCurrentDistances(msg.getData());
+            	
+            	double[] approaching = new double[getCurrentDistances().length];
+            	for (int i = 0; i < getCurrentDistances().length; i++) {
+            		if (getCurrentDistances()[i] < getPreviousDistances()[i]) {
+            			approaching[i] = 1;
+            		}
+            		else if (getCurrentDistances()[i] == getPreviousDistances()[i]) {
+            			approaching[i] = 0;
+            		}
+            		else {
+            			approaching[i] = -1;
+            		}
+            	}
+            	
+            	boolean toggleBlocked = false;
+            	for (int i = 0; i < getCurrentDistances().length; i++) {
+            		if (getCurrentDistances()[i] < 0.75 && approaching[i] == 1) {
+            			setBotBlocked(true);
+            			toggleBlocked = true;
+            		}
+            	}
+            	if (!toggleBlocked) {
+            		setBotBlocked(false);
+            	}
+            	
+            	
             	break;
             default:
                 System.out.println("Error in AP2DX.reflex.Program.componentLogic(Message message) Couldn't deal with message: " + message.getMsgType());
@@ -82,12 +114,106 @@ public class Program extends AP2DXBase
 				e.printStackTrace();
 			}
 			
+			boolean interrupted = false;
+			while (!interrupted) {
+				while (isBotBlocked) {
+					Thread.sleep(10);
+				}
+				Message msg = null;
+				try {
+					msg = getMotorBacklog().take();
+				} catch (InterruptedException e) {
+					interrupted = true;
+				}
+				
+				if (!interrupted && msg != null) {
+					msg.setDestinationModuleId(Module.MOTOR);
+					conn.sendMessage(msg); 
+				}
+				
+			}
+			
+        	
 
 		}
 	}
 
-	public boolean isPlannerBlocked() {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * @param currentDistances the currentDistances to set
+	 */
+	public void setCurrentDistances(double[] currentDistances) {
+		this.currentDistances = currentDistances;
+	}
+
+
+	/**
+	 * @return the currentDistances
+	 */
+	public double[] getCurrentDistances() {
+		return currentDistances;
+	}
+
+
+	/**
+	 * @param previousDistances the previousDistances to set
+	 */
+	public void setPreviousDistances(double[] previousDistances) {
+		this.previousDistances = previousDistances;
+	}
+
+
+	/**
+	 * @return the previousDistances
+	 */
+	public double[] getPreviousDistances() {
+		return previousDistances;
+	}
+
+
+	/**
+	 * @param approaching the approaching to set
+	 */
+	public void setApproaching(double[] approaching) {
+		this.approaching = approaching;
+	}
+
+
+	/**
+	 * @return the approaching
+	 */
+	public double[] getApproaching() {
+		return approaching;
+	}
+
+
+	/**
+	 * @param isBotBlocked the isBotBlocked to set
+	 */
+	public void setBotBlocked(boolean isBotBlocked) {
+		this.isBotBlocked = isBotBlocked;
+	}
+
+
+	/**
+	 * @return the isBotBlocked
+	 */
+	public boolean isBotBlocked() {
+		return isBotBlocked;
+	}
+
+
+	/**
+	 * @param motorBacklog the motorBacklog to set
+	 */
+	public void setMotorBacklog(ArrayBlockingQueue<AP2DXMessage> motorBacklog) {
+		this.motorBacklog = motorBacklog;
+	}
+
+
+	/**
+	 * @return the motorBacklog
+	 */
+	public ArrayBlockingQueue<AP2DXMessage> getMotorBacklog() {
+		return motorBacklog;
 	}
 }
