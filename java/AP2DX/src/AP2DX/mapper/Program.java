d18f4cb08048484439d1fcede53ef3c942befa17
Maarten Inja <maarten.inja@gmail.com>
2011-06-28 18:27:49 +0200

    added mapper and ... slam.exe

--- java/AP2DX/src/AP2DX/mapper/Program.java
+++ java/AP2DX/src/AP2DX/mapper/Program.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import AP2DX.*;
 import AP2DX.specializedMessages.*;
 
+
 public class Program extends AP2DXBase 
 {
 
@@ -21,6 +22,7 @@ public class Program extends AP2DXBase
     public void doOverride()
     {
         slammer = new Slammer();        
+        new Thread(slammer).start();
     }
 	
 	/**
